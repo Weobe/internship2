771623df004bfbf8ebec823bd03f6dc7e96baacc
Weobe <duruozer13@gmail.com>
2023-08-24 20:45:08 +0000

    Upload files

---
+++ test2.py
@@ -0,0 +1,5 @@
+domain = input("Please specify a domain for your question: ")
+
+system_prompt = "You are an advanced assistant that excels at the domain of {}".format(domain)
+
+print(system_prompt)
