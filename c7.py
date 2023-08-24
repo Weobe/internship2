d504a8ec20db81f9592119a75234da21396d6de3
Weobe <duruozer13@gmail.com>
2023-08-24 20:50:36 +0000

    Upload files

---
+++ c7.py
@@ -0,0 +1,10 @@
+from c4 import *
+
+from langchain import HuggingFacePipeline
+from langchain import PromptTemplate,  LLMChain
+
+
+llm = HuggingFacePipeline(pipeline = pipe, model_kwargs = {'temperature':0})
+llm2 = HuggingFacePipeline(pipeline = pipe2, model_kwargs = {'temperatue':0})
+
+
