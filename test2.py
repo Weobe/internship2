ca51afca049a203173a441ed3ea7c320d2f311c0
Weobe <77776599+Weobe@users.noreply.github.com>
2023-08-29 14:37:06 +0300

    Delete test2.py

--- test2.py
+++
@@ -1,5 +0,0 @@
-domain = input("Please specify a domain for your question: ")
-
-system_prompt = "You are an advanced assistant that excels at the domain of {}".format(domain)
-
-print(system_prompt)
