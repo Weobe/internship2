ddcad22ac1435a879c20bf1a3b7cbe0edacb7008
Weobe <77776599+Weobe@users.noreply.github.com>
2023-08-29 11:52:34 +0300

    Add comments

--- pdf.py
+++ pdf.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
-from c6 import *
-from c7 import *
+from load_sentence_transformer import *
+from pdf_init import *
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
@@ -9,6 +9,7 @@ from text import wrap_text
 
 
 def read_pdf():
+    #load document
     pdf_name = input("Enter pdf name: ")
     if not pdf_name.endswith(".pdf"):
         pdf_name = pdf_name + ".pdf"
@@ -18,12 +19,17 @@ def read_pdf():
         )
 
     docs = loader.load()
+
+    #split text and store embedding in vectorstore
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
     all_splits = text_splitter.split_documents(docs)
 
     vectorstore = FAISS.from_documents(documents=all_splits, embedding=HuggingFaceEmbeddings())
 
+    #initialize a Q&A chain with the main LLM and vectorstore of the pdf
     qa_chain = RetrievalQA.from_chain_type(llm,retriever=vectorstore.as_retriever())
+
+    #answer questions from the pdf until user types 'exit'
     while True:
         os.system('clear')
         question = input("Ask a question: ")
@@ -31,7 +37,10 @@ def read_pdf():
             return
         ans = qa_chain({"query": question})
         txt = ans['result']
+        
+        #format text for better readability in terminal
         wrapped_txt = wrap_text(txt, 60)
+        
         print("\n" + wrapped_txt)
         input("\nPress enter to continue...")
 
