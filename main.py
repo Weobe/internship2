c2d407d69a716080e82b589d7c420abb24dd7a50
Weobe <77776599+Weobe@users.noreply.github.com>
2023-08-29 11:44:52 +0300

    Update and rename c5.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from c4 import *
+from format_helper import *
 from langchain import HuggingFacePipeline
 from langchain import PromptTemplate,  LLMChain
 import os
@@ -10,6 +10,10 @@ llm2 = HuggingFacePipeline(pipeline = pipe2, model_kwargs = {'temperature':0})
 
 while True:
     os.system("clear")
+
+    #ask for the domain of the chat and add it to system prompt for higher quality answers
+    #pdf option to use the document understanding feature
+    
     domain = input("Please specify a domain for your question: ")
     if domain == "pdf":
         read_pdf()
@@ -20,14 +24,20 @@ while True:
             instruction = "{text}"
         else:
             instruction = "Please answer this question in the context of " + domain + ". {text}. "#Your answer should depend solely on mathematical modeling"
-        
+
+        #print the instruction and system prompt for transparency
         print(instruction)
         print(system_prompt)
+
+        #create a template that merges instruction and system prompt
         template = get_prompt(instruction, system_prompt)
         print(template)
         prompt = PromptTemplate(template=template, input_variables=["text"])
+
+        #create a chain with the template
         llm_chain = LLMChain(prompt=prompt, llm=llm)
 
+        #answer questions in the current domain until user types 'exit'
         while True:
             os.system("clear")
             text = input("Enter question: ")
