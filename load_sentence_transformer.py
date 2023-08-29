e92b8a4d828ea6d2886b433e37b2de7567c5b750
Weobe <77776599+Weobe@users.noreply.github.com>
2023-08-29 11:46:54 +0300

    Rename and add comments

--- load_sentence_transformer.py
+++ load_sentence_transformer.py
@@ -1,8 +1,10 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 
+#replace model name to use another model
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
 model_kwargs = {}
 encode_kwargs = {'normalize_embeddings': False}
+
 hf = HuggingFaceEmbeddings(
         model_name=model_name,
         model_kwargs=model_kwargs,
