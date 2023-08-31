f801153280346dbceeaba90a21f861d5deff5550
Weobe <77776599+Weobe@users.noreply.github.com>
2023-08-31 13:46:42 +0300

    Update pdf_init.py

--- pdf_init.py
+++ pdf_init.py
@@ -6,6 +6,8 @@ from langchain import PromptTemplate,  LLMChain
 
 
 llm = HuggingFacePipeline(pipeline = pipe, model_kwargs = {'temperature':0})
+
+# isn't used, can be commented
 llm2 = HuggingFacePipeline(pipeline = pipe2, model_kwargs = {'temperatue':0})
 
 
